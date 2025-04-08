a4ea9eb5ef500c80994e1ace9cae513cfd4cd081
Alexander Wikner <awikner@uchicago.edu>
2025-04-08 14:40:10 -0500

    Added new env to example_submission. Updated permissions for setup.

--- setup_material_characterize.sh
+++ setup_material_characterize.sh
@@ -3,4 +3,4 @@
 # load rcc modules
 module load python/miniforge-24.1.2  # python 3.10
 
-source /project/ai4s-hackathon/ai-sci-hackathon-2025/envs/gnnpytorch/bin/activate
+source activate /project/ai4s-hackathon/ai-sci-hackathon-2025/envs/gnnpytorch
