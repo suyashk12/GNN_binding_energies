1ce8041a567f34c2d2c97ab8a8bc30563d39894a
Alexander Wikner <44787151+awikner@users.noreply.github.com>
2025-04-08 12:56:44 -0500

    Removed other environment text

--- setup_material_characterize.sh
+++ setup_material_characterize.sh
@@ -4,5 +4,3 @@
 module load python/miniforge-24.1.2  # python 3.10
 
 source /project/ai4s-hackathon/ai-sci-hackathon-2025/envs/gnnpytorch/bin/activate
-# If you want to develop with JAX instead of torch, use
-# source /project/dfreedman/hackathon/hackathon-env-jax/bin/activate
