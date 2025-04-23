3d215e7c952510fe0d1068033306a1bb682a3582
Shrihan <shrihan@uchicago.edu>
2025-04-23 18:40:35 -0500

    pre-embed version also beats lookup

---
+++ gnn_adapted_v1/gnn_shrihan_explore/helper.py
@@ -0,0 +1,43 @@
+import json
+import networkx as nx
+
+def load_data_from_file(filename, edges=True):
+    """
+    Load a dictionary of graphs from JSON file.
+    """
+    with open(filename, "r") as file_handle:
+        string_dict = json.load(file_handle)
+    return _load_data_from_string_dict(string_dict, edges=edges)
+
+def load_data_from_string(json_string):
+    """
+    Load a dictionary of graphs from JSON string.
+    """
+    string_dict = json.loads(json_string)
+    return _load_data_from_string_dict(string_dict)
+
+def _load_data_from_string_dict(string_dict, edges=True):
+    result_dict = {}
+    for key in string_dict:
+        if edges:
+            graph = nx.node_link_graph(string_dict[key], edges="edges")
+        else:
+            graph = nx.node_link_graph(string_dict[key], edges="links")
+        result_dict[key] = graph
+    return result_dict
+
+def write_data_to_json_string(graph_dict, **kwargs):
+    """
+    Write dictionary of graphs to JSON string.
+    """
+    edges_link_data  = lambda x: nx.node_link_data(x, edges="edges")
+    json_string = json.dumps(graph_dict, default=edges_link_data, **kwargs)
+    return json_string
+
+
+def write_data_to_json_file(graph_dict, filename, **kwargs):
+    """
+    Write dictionary of graphs to JSON file.
+    """
+    with open(filename, "w") as file_handle:
+        file_handle.write(write_data_to_json_string(graph_dict, **kwargs))
